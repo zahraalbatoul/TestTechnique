d73b3154cde80098d76a6048f4afebc9e9143fd9
zahraalbatoul <96121095+zahraalbatoul@users.noreply.github.com>
2025-11-03 12:27:14 +0000

    Fix welcome page styling with CDN Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="font-sans antialiased bg-gray-50 dark:bg-gray-900">
     <div class="min-h-screen flex flex-col">
